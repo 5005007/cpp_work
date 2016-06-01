64bb6c9440c07935886c2e21ef8d60836e867839
MaximKashapov <gkmr.tj@gmail.com>
2016-06-01 18:27:59 +0300

    version 0.3

--- second_semester/structures/structures.cpp
+++ second_semester/structures/structures.cpp
@@ -1,5 +1,4 @@
-﻿#include "stdafx.h"
-#include <iostream>
+﻿#include <iostream>
 #include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,19 +23,56 @@
 
 struct student
 {
-	char* firstName;
-	char* lastName;
-	char* middleName; 
-	char* address;
-	char* group;
-	char* rate;
+	char firstName[30];
+	char lastName[30];
+	char middleName[30];
+	char address[30];
+	char group[10];
+	char rate[5];
 } s;
 
-FILE *f;
+student fillStudent()
+{
+	student stud;
+	printf("Enter first name: ");
+	scanf("%s", stud.firstName);
+	printf("Enter last name: ");
+	scanf("%s", stud.lastName);
+	printf("Enter middle name: ");
+	scanf("%s", stud.middleName);
+	printf("Enter address: ");
+	scanf("%s", stud.address);
+	printf("Enter group: ");
+	scanf("%s", stud.group);
+	printf("Enter rate: ");
+	scanf("%s", stud.rate);
+	return stud;
+}
 
+int calcFileStructs(FILE* file)
+{
+	fseek(file, 0, 2);
+	int n = ftell(file) / sizeof(student);
+	rewind(file);
+	return n;
+}
+
+FILE* openFile(char* mode)
+{
+	FILE* f = NULL;
+	if (!(f = fopen("students", mode)))
+	{
+		printf("Error while file creating\n");
+	}
+	return f;
+}
 
 int editStudent()
 {
+	showStudents();
+	printf("Select student: ");
+	int n = getNumber();
+
 	return 0;
 }
 
@@ -57,27 +93,44 @@ int sortStudents()
 
 int addStudent()
 {
+	if (FILE* file = openFile("ab"))
+	{
+		fwrite(&fillStudent(), 1, sizeof(student), file);
+		fclose(file);
+		return 1;
+	}
+	return 0;
+}
 
-	printf("Enter first name");
-	scanf("%s", &s.firstName);
+student* readFile(FILE* file, int size)
+{
+	student* list = NULL;
+	if (size > 0)
+	{
+		list = (student *)malloc(size * sizeof(student));
+		for (int i = 0; i < size; i++)
+		{
+			fread(&s, sizeof(student), 1, file);
+			list[i] = s;
+		}
+		rewind(file);
+	}
+	return list;
+}
 
-	printf("Enter last name");
-	scanf("%s", &s.lastName);
-
-	printf("Enter middle name");
-	scanf("%s", &s.middleName);
-
-	printf("Enter address");
-	scanf("%s", &s.address);
-
-	printf("Enter group");
-	scanf("%s", &s.group);
-
-	printf("Enter rate");
-	scanf("%s", &s.rate);
-
-	fwrite(&s, sizeof(s), 1, f);
-
+int showStudents()
+{
+	FILE* file;
+	if (file = openFile("rb"))
+	{
+		int size = calcFileStructs(file);
+		student* list = readFile(file, size);
+		for (int i = 0; i < size; i++)
+		{
+			printf("%d) %s %s %s %s %.10s %.5s\n", i + 1, list[i].lastName, list[i].firstName, list[i].middleName, list[i].address, list[i].group, list[i].rate);
+		}
+		fclose(file);
+	}
 	return 1;
 }
 
@@ -101,64 +154,52 @@ void clearScreen()
 
 int getNumber()
 {
-	clearScreen();
 	printf("1) Add student\n");
 	printf("2) Show students\n");
 	printf("3) Edit student\n");
 	printf("4) Search student\n");
 	printf("5) Sort students\n");
 	printf("6) Delete student\n");
+	printf("0) Exit\n");
 	printf("Please enter choice:");
 	return test(_getch());
 }
 
-int openFile()
-{
-	if (!(f = fopen("students", "a+b")))
-	{
-		printf("Error while file creating\n");
-		return 0;
-	}
-}
-
 int main()
 {
-
-	int rs = openFile();
 start:
-	switch (getNumber())
+	clearScreen();
+	int choice = getNumber();
+	clearScreen();
+	switch (choice)
 	{
 	case 0:
-		printf("Good bye!");
+		printf("Good bye!\n");
+		system("pause");
 		break;
 	case 1:
 		addStudent();
 		goto start;
-		break;
 	case 2:
-
-		break;
+		showStudents();
+		system("pause");
+		goto start;
 	case 3:
 		editStudent();
 		goto start;
-		break;
 	case 4:
 		searchStudent();
 		goto start;
-		break;
 	case 5:
 		sortStudents();
 		goto start;
-		break;
 	case 6:
 		deleteStudent();
 		goto start;
-		break;
 	default:
-		printf("Error! Please enter from 1 to 8.");
+		printf("Please enter from 0 to 6\n");
+		system("pause");
 		goto start;
 	}
-	system("pause");
-
 	return 0;
 }
